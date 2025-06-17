37fdd27ca2909fab33bad1dab77f5bae07976deb
LZL0609 <lucy123312@gmail.com>
2025-06-18 00:29:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -43,6 +43,6 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=reply)
     )
-
+port = int(os.environ.get("PORT", 5000))
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
