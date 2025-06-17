9e6cf577e0ba9aa93ee70e2eb4be46bcae0a9ddc
LZL0609 <lucy123312@gmail.com>
2025-06-18 01:07:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import os
 
@@ -12,11 +13,11 @@ balance = 0
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers['X-Line-Signature']
+    signature = request.headers.get('X-Line-Signature')
     body = request.get_data(as_text=True)
     try:
         handler.handle(body, signature)
-    except:
+    except InvalidSignatureError:
         abort(400)
     return 'OK'
 
@@ -43,6 +44,7 @@ def handle_message(event):
         event.reply_token,
         TextSendMessage(text=reply)
     )
-port = int(os.environ.get("PORT", 5000))
+
 if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
